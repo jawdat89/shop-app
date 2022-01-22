ee1fca80984521be3f12bd28ac05f27f973f3be9
Jawdat <Jawdat.89@gmail.com>
2022-01-22 23:46:43 +0200

    fonts loaded successfully

--- shop-app/App.js
+++ shop-app/App.js
@@ -1,4 +1,7 @@
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
@@ -14,11 +17,20 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer);
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <ShopNavigator />
-    </Provider>
-  );
+  let [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <Provider store={store}>
+        <ShopNavigator />
+      </Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
